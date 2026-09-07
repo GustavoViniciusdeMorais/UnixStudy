6327ab044006cff9f5c0b79443d714ee6c5d1044
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2026-09-07 14:05:51 -0300

    - more commands to cli banner

--- scripts/banner.sh
+++ scripts/banner.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-ip -c a
-figlet -f big "LINUX"
-apt search gobuster
-docker ps
+ip -c a | tail -n 10 && \
+figlet -f big LINUX && \
+ip -c l && \
+figlet PHP && \
+tree /home/gustavo/Studies/bashes && \
+echo "MY UPTIME: $(uptime)" && \
+figlet -f small SQL && \
+for val in {1..2}; do echo '{"name":"Kali Linux"}' | jq . ; done
+service ollama status
+
